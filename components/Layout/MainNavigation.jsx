663762e89a437dd35561ca0608278ba80eb0c56a
Arvind <thakurarvindkr10@gmail.com>
2023-05-25 07:02:16 +0530

    Set the token saved in the context to null Redirect the user to login page.

--- components/Layout/MainNavigation.jsx
+++ components/Layout/MainNavigation.jsx
@@ -1,12 +1,17 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
 import { useContext } from 'react';
 import AuthContext from '../../store/auth-context';
 
 const MainNavigation = () => {
+  const history = useHistory();
   const authContext = useContext(AuthContext);
   const isloggedIn = authContext.isLoggedIn;
+  const logoutHandler = () => {
+    authContext.logout();
+    history.replace("/");
+  }
   return (
     <header className={classes.header}>
       <Link to='/'>
@@ -22,7 +27,7 @@ const MainNavigation = () => {
             <Link to='/profile'>Profile</Link>
           </li>}
          { isloggedIn &&  <li>
-            <button>Logout</button>
+            <button onClick={logoutHandler}>Logout</button>
           </li>}
         </ul>
       </nav>
